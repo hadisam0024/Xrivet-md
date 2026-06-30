2195e936e3188ddca136538f3c63354cd2b7ff3e
Cyber Rivet <89631684+hadisam0024@users.noreply.github.com>
2026-06-30 09:48:46 +0500

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
     // 1. CONFIGURATION DE BASE (Session & Database)
     // ===========================================================
     SESSION_ID: process.env.SESSION_ID || "MINI BOT", 
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://offarslan_db_user:arslanmd@cluster0.xrqkzwg.mongodb.net/?appName=Cluster0',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://Hadii09:<db_password>@cluster0.qe7f54x.mongodb.net/?appName=Cluster0',
     
     // ===========================================================
     // 2. INFORMATIONS DU BOT
